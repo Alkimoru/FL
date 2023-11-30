b30eb744d112f8945dd3fe1b6c7b3f5dc4e9b0ae
Alkimoru <135190551+Alkimoru@users.noreply.github.com>
2023-11-30 19:57:33 +0300

    Все задания

---
+++ Лекция_1/Задание_1_1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a == b)
+{
+    Console.Write("Число ");
+    Console.Write(a);
+    Console.Write(" равно числу ");
+    Console.Write(b);
+}
+else {
+    if(a > b)
+    {
+        Console.Write("Число ");
+        Console.Write(a);
+        Console.Write(" больше, чем число ");
+        Console.Write(b);
+    }
+    else{
+        Console.Write("Число ");
+        Console.Write(b);
+        Console.Write(" больше, чем число ");
+        Console.Write(a);
+    }
+}
